95ba38855b3baf4e33eb40bb6c6f23ad850f0c15
Thiago <thiagofreitascarneiro@hotmail.com>
2021-12-11 16:54:45 -0300

    creating Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import styles from './styles.module.scss'
+
+
+export function Header() {
+    return (
+        <header className = {styles.HeaderContainer}>
+            <div className= {styles.headerContent}>
+               <img src="/images/logo.svg" alt="ig.news"/> 
+
+               <nav>    
+                   <a className={styles.active}>Home</a>
+                   <a>Posts</a>
+
+               </nav>
+            </div>
+        </header>
+    )
+
+
+}
