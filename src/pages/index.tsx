d1fee8976287fe7bb57e4185ea20351d85b4be2e
Thiago <thiagofreitascarneiro@hotmail.com>
2022-01-09 18:56:32 -0300

    configuring prismic.io int the project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,6 +43,10 @@ export default function Home({ product }) {
   )
 }
 
+/*export const getServerSideProps = ({ req }) => {
+
+}*/
+
 export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1K7qOyKYaULAQNBFCIEA36WG', {
     expand: ['product']
---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,55 @@
+import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import { getPrismicClient } from '../../services/prismic';
+import styles from './styles.module.scss';
+import Prismic from '@prismicio/client'
+
+export default function Posts() {
+    return (
+        <>
+            <Head>
+                <title>Posts | Ignews </title>
+            </Head>
+
+            <main className={styles.container}>
+                <div className={styles.posts}>
+                    <a href='#'>
+                        <time> 9 de Janeiro de 2021 </time>
+                        <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+                        <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared</p>
+                    </a>
+                    <a href='#'>
+                        <time> 9 de Janeiro de 2021 </time>
+                        <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+                        <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared</p>
+                    </a>
+                    <a href='#'>
+                        <time> 9 de Janeiro de 2021 </time>
+                        <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+                        <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared</p>
+                    </a>
+                </div>
+            </main>
+
+
+        
+        </>
+    );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    const prismic = getPrismicClient()
+
+    const response = await prismic.query([
+        Prismic.predicates.at('document.type', 'Post')
+    ], {
+        fetch: ['post.title', 'post.content'],
+        pageSize: 100,
+    })
+
+    console.log('response', JSON.stringify(response, null, 2))
+
+    return  {
+        props: {}
+    }
+}
