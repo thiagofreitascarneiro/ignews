2ddd6a7670a9f54069f26a565d05d96060aab15d
Thiago <thiagofreitascarneiro@hotmail.com>
2022-01-11 23:28:35 -0300

    using SPA - NextJS to render Header Index/Posts

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import { SignInButton } from '../SigninButton'
 import styles from './styles.module.scss'
 
+import Link from 'next/link'
 
 export function Header() {
     return (
@@ -8,9 +9,14 @@ export function Header() {
             <div className= {styles.headerContent}>
                <img src="/images/logo.svg" alt="ig.news"/> 
 
-               <nav>    
-                   <a className={styles.active}>Home</a>
-                   <a>Posts</a>
+               <nav>
+                   <Link href="/">    
+                        <a className={styles.active}>Home</a>
+                   </Link>
+                   <Link href="/posts">
+                        <a>Posts</a>
+                   </Link>
+                
 
                </nav>
 
