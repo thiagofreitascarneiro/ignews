2ebde4a44b901ec4f9e8246750c26cb02eba9a55
thiago <thiagofreitascarneiro@hotmail.com>
2022-05-04 10:41:41 -0300

    creating test at post page

--- src/tests/pages/Posts.spec.tsx
+++ src/tests/pages/Posts.spec.tsx
@@ -3,9 +3,10 @@ import { mocked } from 'jest-mock';
 import Posts, { getStaticProps, Post } from '../../pages/posts';
 import { getPrismicClient } from '../../services/prismic';
 
+jest.mock('../../services/stripe')
 
 const posts = [
-    { 
+    {
         slug: 'fake-slug',
         title: 'Fake title 1',
         excerpt: 'Fake excerpt 1',
@@ -13,80 +14,56 @@ const posts = [
     }
 ] as Post[];
 
-jest.mock('../../services/prismic', () => {
-    return {
-      client: {
-        getAllByType: jest.fn(),
-      },
-    };
-  });
+jest.mock('../../services/prismic')
 
-  describe("Posts page", () => {
-    it("renders correctly", () => {
-      render(<Posts posts={posts} />);
-      const getPrismicClientMocked = mocked(getPrismicClient);
-      getPrismicClientMocked.mockReturnValueOnce({
-        getAllByType: jest.fn().mockResolvedValueOnce(
-            [
-                {
-                    uid: 'fake-slug',
-                    data: {
-                        title: 'Fake title 1',
-                        content: [
-                            {
-                                type: 'paragraph',
-                                text: 'Fake excerpt 1',
-                            },
-                        ],
-                    },
-                    last_publication_date: '2020-01-01',
-                },
-            ],
-        ),
-    } as any);
-      expect(screen.getByText("Fake title 1")).toBeInTheDocument();
+describe('Posts page', () => {
+
+    it('renders correctly', () => {
+        render(<Posts posts={posts} />);
+
+        expect(screen.getByText('Fake title 1')).toBeInTheDocument();
     });
 
-    it("loads initial data", async () => {
-    const getPrismicClientMocked = mocked(getPrismicClient);
+    it('loads initial data', async () => {
+        const getPrismicClientMocked = mocked(getPrismicClient);
 
-    getPrismicClientMocked.mockReturnValueOnce([
-      {
-        uid: "fake-slug",
-        data: {
-          title: [
-            {
-              type: "heading1",
-              text: "Fake title 1",
-            },
-          ],
-          content: [
-            {
-              type: "paragraph",
-              text: "Fake excerpt 1",
-            },
-          ],
-        },
-        last_publication_date: "2022-04-22T03:00:00.000Z",
-      },
-    ] as any);
+        getPrismicClientMocked.mockReturnValueOnce({
+            getAllByType: jest.fn().mockResolvedValueOnce(
+                [
+                    {
+                        uid: 'fake-slug',
+                        data: {
+                            title: 'Fake title 1',
+                            content: [
+                                {
+                                    type: 'paragraph',
+                                    text: 'Fake excerpt 1',
+                                },
+                            ],
+                        },
+                        last_publication_date: '2020-01-01',
+                    },
+                ],
+            ),
+        } as any);
 
-    const response = await getStaticProps({ previewData: undefined });
+        const response = await getStaticProps({
+            previewData: undefined,
+        });
 
-
-    expect(response).toEqual(
-      expect.objectContaining({
-        props: {
-          data: [
-            {
-              slug: "fake-slug",
-              title: "Fake title 1",
-              excerpt: "Fake excerpt 1",
-              updatedAt: "01 de janeiro de 2020",
-            },
-          ],
-        },
-      })
-    );
-  });
+        expect(response).toEqual(
+            expect.objectContaining({
+                props: {
+                    posts: [
+                        {
+                            slug: 'fake-slug',
+                            title: 'Fake title 1',
+                            excerpt: 'Fake excerpt 1',
+                            updated_at: '01 de janeiro de 2020',
+                        }
+                    ]
+                }
+            })
+        )
+    });
 });
